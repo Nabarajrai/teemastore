031c64c7d885bc3eefa5da1e5b79b23d07a20588
Nabaraj <>
2023-07-01 11:43:12 +0545

    added delivery section and card components

--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -3,6 +3,36 @@ import Layout from "../../common/layout/Layout";
 import HomeBannerComponent from "../../components/homeBanner/HomeBanner.component";
 import CategoryComponent from "../../components/category/Category.component";
 import { fetchAllProuducts } from "../../services/api";
+import ButtonComponent from "../../components/button/Button.component";
+import { BUTTON_TYPES } from "../../common/layout/ButtonTypes";
+import DeliveryComponent from "../../components/delivery/Delivery.component";
+import truck from "../../assets/images/truck.png";
+import home from "../../assets/images/home.png";
+import time from "../../assets/images/time.png";
+import kids from "../../assets/images/kids.png";
+
+const deliveryIfno = [
+  {
+    id: 1,
+    title: "FREE US DELIVERY",
+    description:
+      "Free shipping to First-Time customers only, U.S Customers or Order over $200 ",
+    img: `${truck}`,
+  },
+  {
+    id: 2,
+    title: "CREDIT CARD",
+    description:
+      "Free We accept Carte Blanche, Master Card, Visa and American Express ",
+    img: `${home}`,
+  },
+  {
+    id: 3,
+    title: "30 DAYS RETURN",
+    description: "Exchanges are allowed within 30 days of your purchase date ",
+    img: `${time}`,
+  },
+];
 
 const HomePage = () => {
   const [categorys, setCategorys] = useState([]);
@@ -25,6 +55,25 @@ const HomePage = () => {
             {categorys.map(category => (
               <CategoryComponent name={category} />
             ))}
+          </div>
+        </div>
+      </div>
+      <div className="dellivery-section">
+        <div className="wrapper">
+          <div className="delivery-card">
+            {deliveryIfno.map(delivery => (
+              <DeliveryComponent key={delivery.id} deliInfos={delivery} />
+            ))}
+          </div>
+          <div className="delivery-description">
+            <div className="delivery-background">
+              <img src={kids} alt="kids" />
+            </div>
+            <div className="deliery-cta">
+              <ButtonComponent buttonType={BUTTON_TYPES.normal}>
+                Kids tees
+              </ButtonComponent>
+            </div>
           </div>
         </div>
       </div>

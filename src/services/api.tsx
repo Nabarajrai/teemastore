79388e8136589ea5ead3a66f0acc1aa7cf09a5f2
Nabaraj <>
2023-07-02 15:12:46 +0545

    build carts and cutomer cards

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -3,3 +3,6 @@ import axiosInstance from "../utils/apiConfig/apiConfig";
 export const fetchAllProuducts = () => {
   return axiosInstance.get("/products/categories");
 };
+export const getAllProducts = () => {
+  return axiosInstance.get("/products");
+};
